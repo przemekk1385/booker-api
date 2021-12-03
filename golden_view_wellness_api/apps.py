182c2e8fb75d008709f000b43b1ce1c7c775f503
Przemysław Kaliś <przemek.kalis@gmail.com>
2021-12-03 19:04:29 +0100

    Add Django stuff

---
+++ golden_view_wellness_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GoldenViewWellnessApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "golden_view_wellness_api"
--- tests/test_golden_view_wellness_api.py
+++ tests/test_golden_view_wellness_api.py
@@ -2,4 +2,4 @@ from golden_view_wellness_api import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
