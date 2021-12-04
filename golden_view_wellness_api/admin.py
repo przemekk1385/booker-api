a749619186b74f2ebda680477bb24a6d7f3e8772
Przemysław Kaliś <przemek.kalis@gmail.com>
2021-12-04 18:09:26 +0100

    Add models to admin panel

--- golden_view_wellness_api/admin.py
+++ golden_view_wellness_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from golden_view_wellness_api.models import Booking, Stay
+
+admin.site.register(Booking)
+admin.site.register(Stay)
