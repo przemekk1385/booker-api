bbd123c854f182f9e80eeae8a83ab7dad5d75363
Przemysław Kaliś <przemek.kalis@gmail.com>
2021-12-08 21:34:53 +0100

    Enhance app config

--- booker_api/apps.py
+++ booker_api/apps.py
@@ -1,6 +1,10 @@
+import environ
 from django.apps import AppConfig
+
+env = environ.Env(B_DAYS_BETWEEN=(int, 1))
 
 
 class BookerApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "booker_api"
+    days_between_bookings = env("B_DAYS_BETWEEN")
