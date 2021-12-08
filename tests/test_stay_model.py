05f82c0f6673e48f2eb6338a7b43faf060d926da
Przemysław Kaliś <przemek.kalis@gmail.com>
2021-12-08 21:57:06 +0100

    Stay limit (#3)
    * Cleanup migrations
    
    * Prevent using more than one identifier for stay

---
+++ tests/test_stay_model.py
@@ -0,0 +1,24 @@
+from datetime import date, timedelta
+
+import pytest
+from django.db import IntegrityError
+
+from booker_api.models import Stay
+
+
+@pytest.mark.django_db
+def test_unique_constraint(faker):
+    stay_instance = Stay.objects.create(
+        apartment=faker.random_element(Stay.Apartment),
+        identifier=faker.numerify("#########"),
+        date_from=date.today(),
+        date_to=date.today() + timedelta(days=7),
+    )
+
+    with pytest.raises(IntegrityError):
+        Stay.objects.create(
+            apartment=stay_instance.apartment,
+            identifier=faker.numerify("#########"),
+            date_from=stay_instance.date_from,
+            date_to=stay_instance.date_to,
+        )
