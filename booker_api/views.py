7c1683a7c8c70e12b98a490860f76637bac16677
Przemysław Kaliś <przemek.kalis@gmail.com>
2021-12-22 17:09:23 +0100

    Use serializer for request validation

--- booker_api/views.py
+++ booker_api/views.py
@@ -1,84 +1,59 @@
 from datetime import date, timedelta
 
-from django.utils.dateparse import parse_date
-from rest_framework import status, views
+from rest_framework import serializers, status, views
 from rest_framework.decorators import api_view
-from rest_framework.exceptions import ParseError
-from rest_framework.fields import CharField, DateField, Field
 from rest_framework.generics import get_object_or_404
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from booker_api.models import IDENTIFIER_MAX_LENGTH, Booking
 from booker_api.serializers import BookingSerializer, SlotSerializer
 
 
+class BookingDeleteRequest(serializers.Serializer):
+    day = serializers.DateField()
+    identifier = serializers.CharField(max_length=IDENTIFIER_MAX_LENGTH)
+
+
 class BookingView(views.APIView):
     serializer_class = BookingSerializer
 
-    def _validate_delete_request_body(self):
-        data = self.request.data
-
-        errors = {
-            field: [Field.default_error_messages["required"]]
-            for field in ["day", "identifier"]
-            if field not in data
-        }
-
-        if errors:
-            raise ParseError(detail=errors)
-        else:
-            try:
-                day = parse_date(data["day"])
-            except (ValueError, TypeError):
-                errors["day"] = DateField.default_error_messages["invalid"]
-            else:
-                if day is None:
-                    errors["day"] = DateField.default_error_messages["invalid"]
-
-            identifier = data["identifier"]
-            if len(identifier) > IDENTIFIER_MAX_LENGTH:
-                errors["identifier"] = CharField.default_error_messages["max_length"]
-
-        if errors:
-            raise ParseError(detail=errors)
-
-        return day, identifier
-
-    def get_object(self):
-        day, identifier = self._validate_delete_request_body()
-
+    def get_object(self, day: date, identifier: str) -> Booking:
         queryset = self.get_queryset()
-        obj = get_object_or_404(queryset, day=str(day), stay__identifier=identifier)
+        obj = get_object_or_404(queryset, day=day, stay__identifier=identifier)
         return obj
 
     def get_queryset(self):
         return Booking.objects.filter(day__gte=date.today() - timedelta(days=1))
 
-    def get(self, _):
+    def get(self, request: Request) -> Response:
         queryset = self.get_queryset()
         serializer = self.serializer_class(queryset, many=True)
 
         return Response(serializer.data)
 
-    def post(self, request):
+    def post(self, request: Request) -> Response:
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def delete(self, _):
-        instance = self.get_object()
+    def delete(self, request: Request) -> Response:
+        delete_request = BookingDeleteRequest(data=request.data)
+        delete_request.is_valid(raise_exception=True)
+
+        instance = self.get_object(**delete_request.data)
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(("GET",))
-def health_status(request):
+def health_status(request: Request) -> Response:
     return Response({}, status=status.HTTP_200_OK)
 
 
 @api_view(["GET"])
-def slot_list(request):
+def slot_list(request: Request) -> Response:
     serializer = SlotSerializer(Booking.Slot, many=True)
 
     return Response(data=serializer.data)
