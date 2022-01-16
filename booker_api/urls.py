c94f1172e7586b30e8a7054b674a0ed54d22ca94
Przemysław Kaliś <przemek.kalis@gmail.com>
2022-01-16 22:16:35 +0100

    Rename view

--- booker_api/urls.py
+++ booker_api/urls.py
@@ -11,5 +11,5 @@ app_name = AppConfig.name
 urlpatterns = [
     path(f"{API_VERSION_PREFIX}/", include(router.urls)),
     re_path(fr"^{API_VERSION_PREFIX}/slot/$", views.slot_list, name="slot-list"),
-    re_path(fr"^{API_VERSION_PREFIX}/health/$", views.slot_list, name="health"),
+    re_path(fr"^{API_VERSION_PREFIX}/health/$", views.health, name="health"),
 ]
