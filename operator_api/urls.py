ce1d368469cf1067ea8ae3a3a3e0d1e0e8136360
Przemysław Kaliś <przemek.kalis@gmail.com>
2022-01-16 22:18:33 +0100

    Add protected health endpoint

--- operator_api/urls.py
+++ operator_api/urls.py
@@ -1,5 +1,6 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 
+from operator_api import views
 from operator_api.apps import OperatorApiConfig as AppConfig
 from operator_api.routers import router
 
@@ -9,4 +10,5 @@ app_name = AppConfig.name
 
 urlpatterns = [
     path(f"{API_VERSION_PREFIX}/", include(router.urls)),
+    re_path(fr"^{API_VERSION_PREFIX}/health/$", views.health, name="health"),
 ]
