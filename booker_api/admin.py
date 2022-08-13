ba70b52dd380505e43f6eb3369421daa56982c70
Przemysław Kaliś <przemek.kalis@gmail.com>
2022-08-13 18:13:49 +0200

    Enhanced delete permissions (#20)
    * Update hooks
    
    * Fix tests
    
    * Disallow delete passed bookings
    
    * Bump black

--- booker_api/admin.py
+++ booker_api/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
+from django.core.handlers.wsgi import WSGIRequest
 
 from booker_api.models import Apartment, Booking
-
-admin.site.register(Booking)
+from booker_api.utils import get_now
 
 
 @admin.register(Apartment)
 class ApartmentAdmin(admin.ModelAdmin):
     fields = ["code", "number"]
     readonly_fields = ["number"]
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request: WSGIRequest, obj: Booking = None):
+        now = get_now()
+
+        return (
+            True
+            if not obj
+            else any(
+                [obj.day == now.date() and obj.slot > now.hour, obj.day > now.date()]
+            )
+        )
