db80273b06c02a401284b765ba0a39c99fca73a2
Przemysław Kaliś <przemek.kalis@gmail.com>
2022-08-15 08:16:44 +0200

    Revert delete restrictions (#21)

--- booker_api/admin.py
+++ booker_api/admin.py
@@ -1,25 +1,11 @@
 from django.contrib import admin
-from django.core.handlers.wsgi import WSGIRequest
 
 from booker_api.models import Apartment, Booking
-from booker_api.utils import get_now
+
+admin.site.register(Booking)
 
 
 @admin.register(Apartment)
 class ApartmentAdmin(admin.ModelAdmin):
     fields = ["code", "number"]
     readonly_fields = ["number"]
-
-
-@admin.register(Booking)
-class BookingAdmin(admin.ModelAdmin):
-    def has_delete_permission(self, request: WSGIRequest, obj: Booking = None):
-        now = get_now()
-
-        return (
-            True
-            if not obj
-            else any(
-                [obj.day == now.date() and obj.slot > now.hour, obj.day > now.date()]
-            )
-        )
